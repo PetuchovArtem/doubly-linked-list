b224eafca49f354a4652b035051076da3657995d
PetuchovArtem <petuchov.artem@gmail.com>
2017-01-26 23:14:04 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,34 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor(value) {
+    const list = new LinkedList();        
+    this._length = 0;
+    this.head = null;
+    this.tail = null;
+}
 
-    append(data) {}
+    append(data) {
+        }
 
     head() {}
 
     tail() {}
 
-    at(index) {}
+    at(index) {
+    var currentNode = this.head,
+        length = this._length,
+        count = 1,
+        message = {failure: 'Failure: non-existent node in this list.'};
+    if (length === 0 || position < 1 || position > length) {
+        throw new Error(message.failure);
+    }
+       while (count < position) {
+        currentNode = currentNode.next;
+        count++;
+    }
+    return currentNode;
+    }
 
     insertAt(index, data) {}
 
